d6125814b32bd11cd50981f87f4e67f1fffba0d0
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-09-21 19:06:22 +0200

    logo

--- src/Requests.js
+++ src/Requests.js
@@ -2,6 +2,7 @@
 const key = process.env.REACT_APP_IMDB_API_KEY
 
 
+
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&languague=en-US&page=1`, 
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&languague=en-US&page=1`, 
