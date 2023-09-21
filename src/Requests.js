4318d75feee9db83b6a593960280405227bc2f50
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-09-21 16:42:07 +0200

    api token change

--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,6 @@
-const key = "2b404baab7e7fc7561e3551d16370b4c"
+// const key = "2b404baab7e7fc7561e3551d16370b4c" do not use the key, already deprecated
+const key = process.env.REACT_APP_IMDB_API_KEY
+
 
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&languague=en-US&page=1`, 
