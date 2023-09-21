6352785b3afd8b87143ad404218283903d9769e2
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-09-21 16:21:45 +0200

    deploy to firebase

--- src/Requests.js
+++ src/Requests.js
@@ -1,14 +1,17 @@
-const key = 'c5b986fca0d87c67d9bc65fda5fb5ae6'
+const key = "2b404baab7e7fc7561e3551d16370b4c"
 
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&languague=en-US&page=1`, 
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&languague=en-US&page=1`, 
     requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&languague=en-US&page=2`,
-    requestHorror: `https://api.themoviedb.ord/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1`, 
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&languague=en-US&page=1`,
+
+    
 };
 
 export default requests;
 
 
-// https://api.themoviedb.org/3/movie/popular?api_key=c5b986fca0d87c67d9bc65fda5fb5ae6&languague=en-US&page=1
+
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Main from '../components/Main'
+import React from 'react';
+import Main from '../components/Main';
+import Row from '../components/Row';
+import requests from '../Requests';
 
 const Home = () => {
   return (
     <div>
-        <Main />
-    </div>
-  )
-}
+      <Main />
+      <Row rowID="1" title="UpComing" fetchURL={requests.requestUpcoming} />
+      <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <p className='text-[red] text-center flex justify-center items-center'>Created by Brigita B for learning purposes.</p>
 
-export default Home
+    </div>
+  );
+};
+
+export default Home;
